fdbe6f059b8f294762b287ae821cc30dbe4cf773
c3eru <yyprjkt@gmail.com>
2022-04-27 12:55:05 +0700

    test

--- build.sh
+++ build.sh
@@ -1,40 +1,7 @@
 #!/usr/bin/env bash
-
- #
- # Script For Building Android Kernel
- #
-
-if [ ! -d "${PWD}/kernel_ccache" ]; 
-    then
-    mkdir -p "${PWD}/kernel_ccache"
-    fi
-    export CCACHE_DIR="${PWD}/kernel_ccache"
-    export CCACHE_EXEC=$(which ccache)
-    export USE_CCACHE=1
-    ccache -M 2G
-    ccache -z
-
-# Specify Kernel Directory
-KERNEL_DIR="$(pwd)"
-
-# Kernel Defconfig
-DEFCONFIG=vendor/citrus-perf_defconfig
-
-# Files
-IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-
-# Date and Time
-DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
-TANGGAL=$(date +"%F%S")
-
-# Specify Final Zip Name
-ZIPNAME=mobx
-FINAL_ZIP=${ZIPNAME}-Kernel-${TANGGAL}.zip
-
-# Clone ToolChain & AnyKernel
-function cloneTC() {
-        mkdir aosp-clang
-        cd aosp-clang || exit
+echo "Downloading few Dependecies . . ."
+        mkdir clang
+        cd clang || exit
         
 	    wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r437112b.tar.gz
         tar -xf clang*
@@ -45,63 +12,80 @@ function cloneTC() {
 	    git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git  --depth=1 gcc32
 
         git clone --depth=1 https://github.com/reaPeR1010/AnyKernel3
-        
-        PATH="${KERNEL_DIR}/aosp-clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
-        make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
-        make -j$(nproc) ARCH=arm64 O=out \
-        CC=clang \
-        CLANG_TRIPLE=aarch64-linux-gnu- \
-	    CROSS_COMPILE=aarch64-linux-android- \
-	    CROSS_COMPILE_COMPAT=arm-linux-androideabi- \
-	    V=$VERBOSE 2>&1 | tee error.log
 
-	# Verify Files
-	if ! [ -a "$IMAGE" ];
-	   then
-	       push "error.log" "Build Throws Errors"
-	       exit 1
-	   else
-	       post_msg " Kernel Compilation Finished. Started Zipping "
-	fi
+# Main
+KERNEL_NAME=$kernel_name # IMPORTANT ! Declare your kernel name
+KERNEL_DIR="$(pwd)" # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_CODENAME=$device_codename # IMPORTANT ! Declare your device codename
+DEVICE_DEFCONFIG=$kernel_defconfig # IMPORTANT ! Declare your kernel source defconfig file here.
+CLANG_ROOTDIR=$(pwd)/clang # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=xyzuan # Change with your own name or else.
+export KBUILD_BUILD_HOST=xyzscape-ci # Change with your own hostname.
+IMAGE=$(pwd)/lavender/out/arch/arm64/boot/Image.gz-dtb
+DATE=$(date +"%F-%S")
+START=$(date +"%s")
+PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
 
+# Checking environtment
+# Warning !! Dont Change anything there without known reason.
+function check() {
+echo ================================================
+echo BUILDER NAME = ${KBUILD_BUILD_USER}
+echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
+echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
+echo CLANG_VERSION = $(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')
+echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
+echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
+echo ================================================
 }
 
+# Compiler
+function compile() {
+    cd ${KERNEL_ROOTDIR}
+    make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
+    make -j$(nproc) ARCH=arm64 O=out \
+	CC=${CLANG_ROOTDIR}/bin/clang \
+	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
 
-function zipping() {
-	# Copy Files To AnyKernel3 Zip
-	cp $IMAGE AnyKernel3
-	
-	# Zipping and Push Kernel
-	cd AnyKernel3 || exit 1
-        zip -r9 ${FINAL_ZIP} *
-        MD5CHECK=$(md5sum "$FINAL_ZIP" | cut -d' ' -f1)
-        push "$FINAL_ZIP" "Build took : $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s) | For <b>$MODEL ($DEVICE)</b> | <b>${KBUILD_COMPILER_STRING}</b> | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
-        cd ..
+   if ! [ -a "$IMAGE" ]; then
+	finerr
+	exit 1
+   fi
+    git clone --depth=1 $anykernel AnyKernel
+	cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 
-
-function post_msg() {
-	curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-	-d chat_id="$chat_id" \
-	-d "disable_web_page_preview=true" \
-	-d "parse_mode=html" \
-	-d text="$1"
-}
-
+# Push
 function push() {
-	curl -F document=@$1 "https://api.telegram.org/bot$token/sendDocument" \
-	-F chat_id="$chat_id" \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$2"
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 7 (lavender)</b> | <b>$(${CLANG_ROOTDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
+
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
 }
 
-
-cloneTC
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 ${KERNEL_NAME}-${DEVICE_CODENAME}-${DATE}.zip *
+    cd ..
+}
+check
+compile
+zipping
 END=$(date +"%s")
 DIFF=$(($END - $START))
-zipping
-
-
-
-
+push
